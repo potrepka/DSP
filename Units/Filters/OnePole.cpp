35dd7b4f13de30e0213dcff6ccc82399dc5f5a7c
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-06-12 13:01:29 -0700

    Add one pole filter

---
+++ Units/Filters/OnePole.cpp
@@ -0,0 +1,49 @@
+#include "OnePole.h"
+
+const std::size_t dsp::OnePole::FREQUENCY = 1;
+
+dsp::OnePole::OnePole() : Processor(Connection::Type::BIPOLAR, Connection::Type::BIPOLAR), mode(Mode::LOW_PASS) {
+    pushInput(Connection::Type::HERTZ);
+}
+
+void dsp::OnePole::setNumChannels(std::size_t numChannels) {
+    lock();
+    setNumChannelsNoLock(numChannels);
+    y1.resize(numChannels, 0);
+    unlock();
+}
+
+dsp::OnePole::Mode dsp::OnePole::getMode() {
+    return mode;
+}
+
+void dsp::OnePole::setMode(Mode mode) {
+    this->mode = mode;
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::OnePole::getFrequency() {
+    return getInput(FREQUENCY);
+}
+
+void dsp::OnePole::process() {
+    Unit::process();
+    for (std::size_t i = 0; i < getNumChannels(); i++) {
+        std::vector<DSP_FLOAT> &inputBuffer = getInputSignal()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &frequencyBuffer = getFrequency()->getChannel(i)->getBuffer();
+        for (unsigned int k = 0; k < getBufferSize(); k++) {
+            DSP_FLOAT a0, b1;
+            switch (mode) {
+                case Mode::LOW_PASS:
+                    b1 = exp(-TAU * frequencyBuffer[k] / getSampleRate());
+                    a0 = 1.0 - b1;
+                    break;
+                case Mode::HIGH_PASS:
+                    b1 = -exp(-TAU * frequencyBuffer[k] / getSampleRate());
+                    a0 = 1.0 + b1;
+                    break;
+            }
+            y1[k] = outputBuffer[k] = a0 * inputBuffer[k] + b1 * y1[k];
+        }
+    }
+}
---
+++ Units/Filters/OnePole.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Processor.h"
+
+namespace dsp {
+
+class OnePole : Processor {
+
+public:
+    enum class Mode { LOW_PASS, HIGH_PASS };
+
+    OnePole();
+    void setNumChannels(std::size_t numChannels) override;
+    Mode getMode();
+    void setMode(Mode mode);
+    std::shared_ptr<InputParameter> getFrequency();
+
+protected:
+    void process() override;
+
+private:
+    static const std::size_t FREQUENCY;
+    Mode mode;
+    std::vector<DSP_FLOAT> y1;
+};
+
+} // namespace dsp
