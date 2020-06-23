573139265cf39f4a9eb021902bb42ff1482f36f0
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-06-23 13:20:26 -0700

    Use pointers for tables in TableOscillator

--- src/Units/Buffer/TableOscillator.cpp
+++ src/Units/Buffer/TableOscillator.cpp
@@ -12,25 +12,25 @@ std::size_t dsp::TableOscillator::getNumTables() {
     return tables.size();
 }
 
-std::vector<DSP_FLOAT> dsp::TableOscillator::getTable(std::size_t index) {
-    return tables[index];
+std::vector<DSP_FLOAT> &dsp::TableOscillator::getTable(std::size_t index) {
+    return *tables[index];
 }
 
-void dsp::TableOscillator::setTable(std::size_t index, const std::vector<DSP_FLOAT> &table) {
+void dsp::TableOscillator::setTable(std::size_t index, std::vector<DSP_FLOAT> &table) {
     lock();
-    tables[index] = table;
+    tables[index] = &table;
     unlock();
 }
 
-void dsp::TableOscillator::pushTable(const std::vector<DSP_FLOAT> &table) {
+void dsp::TableOscillator::pushTable(std::vector<DSP_FLOAT> &table) {
     lock();
-    tables.push_back(table);
+    tables.push_back(&table);
     unlock();
 }
 
-void dsp::TableOscillator::insertTable(std::size_t index, const std::vector<DSP_FLOAT> &table) {
+void dsp::TableOscillator::insertTable(std::size_t index, std::vector<DSP_FLOAT> &table) {
     lock();
-    tables.insert(tables.begin() + index, table);
+    tables.insert(tables.begin() + index, &table);
     unlock();
 }
 
@@ -62,8 +62,8 @@ void dsp::TableOscillator::process() {
                 const long indexBefore = static_cast<long>(positionIndex) - 1;
                 long p = indexBefore;
                 for (unsigned char j = 0; j < 4; j++) {
-                    if (p >= 0 && p < tables.size() && tables[p].size() > 0) {
-                        points[j] = linear(tables[p], wrap(phaseBuffer[k], 0.0, 1.0) * tables[p].size());
+                    if (p >= 0 && p < tables.size() && tables[p]->size() > 0) {
+                        points[j] = linear(*tables[p], wrap(phaseBuffer[k], 0.0, 1.0) * tables[p]->size());
                     } else {
                         points[j] = 0.0;
                     }
--- src/Units/Buffer/TableOscillator.h
+++ src/Units/Buffer/TableOscillator.h
@@ -9,10 +9,10 @@ class TableOscillator : public Generator {
 public:
     TableOscillator();
     std::size_t getNumTables();
-    std::vector<DSP_FLOAT> getTable(std::size_t index);
-    void setTable(std::size_t index, const std::vector<DSP_FLOAT> &table);
-    void pushTable(const std::vector<DSP_FLOAT> &table);
-    void insertTable(std::size_t index, const std::vector<DSP_FLOAT> &table);
+    std::vector<DSP_FLOAT> &getTable(std::size_t index);
+    void setTable(std::size_t index, std::vector<DSP_FLOAT> &table);
+    void pushTable(std::vector<DSP_FLOAT> &table);
+    void insertTable(std::size_t index, std::vector<DSP_FLOAT> &table);
     void removeTable(std::size_t index);
     std::shared_ptr<InputParameter> getPhase();
     std::shared_ptr<InputParameter> getPosition();
@@ -23,7 +23,7 @@ protected:
 private:
     static const std::size_t PHASE;
     static const std::size_t POSITION;
-    std::vector<std::vector<DSP_FLOAT>> tables;
+    std::vector<std::vector<DSP_FLOAT> *> tables;
 };
 
 } // namespace dsp
