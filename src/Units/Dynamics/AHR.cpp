475aef5081119fe351894f0d65cc8c241b9dd818
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-06-27 02:01:05 -0700

    Add attack-hold-release envelope

---
+++ src/Units/Dynamics/AHR.cpp
@@ -0,0 +1,62 @@
+#include "AHR.h"
+
+const std::size_t dsp::AHR::RESET_TRIGGER = 0;
+const std::size_t dsp::AHR::ATTACK = 1;
+const std::size_t dsp::AHR::HOLD = 2;
+const std::size_t dsp::AHR::RELEASE = 3;
+
+dsp::AHR::AHR() : Generator(Connection::Type::UNIPOLAR), samples(0) {
+    pushInput(Connection::Type::BINARY);
+    pushInput(Connection::Type::SECONDS);
+    pushInput(Connection::Type::SECONDS);
+    pushInput(Connection::Type::SECONDS);
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::AHR::getResetTrigger() {
+    return getInput(RESET_TRIGGER);
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::AHR::getAttack() {
+    return getInput(ATTACK);
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::AHR::getHold() {
+    return getInput(HOLD);
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::AHR::getRelease() {
+    return getInput(RELEASE);
+}
+
+void dsp::AHR::setNumChannelsNoLock(std::size_t numChannels) {
+    Unit::setNumChannelsNoLock(numChannels);
+    position.resize(numChannels, 0);
+    value.resize(numChannels, 1.0);
+}
+
+void dsp::AHR::process() {
+    Unit::process();
+    for (std::size_t i = 0; i < getNumChannels(); i++) {
+        std::vector<DSP_FLOAT> &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &attackBuffer = getAttack()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &holdBuffer = getHold()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &releaseBuffer = getRelease()->getChannel(i)->getBuffer();
+        for (unsigned int k = 0; k < getBufferSize(); k++) {
+            if (resetTriggerBuffer[k]) {
+                position[i] = 0;
+                value[i] = 1.0;
+            }
+            DSP_FLOAT attackPosition = attackBuffer[k] * getSampleRate();
+            if (position[i] < attackPosition) {
+                value[i] = 1.0;
+                outputBuffer[k] = position[i] / attackPosition;
+            } else if (position[i] < (attackBuffer[k] + holdBuffer[k]) * getSampleRate()) {
+                outputBuffer[k] = value[i] = 1.0;
+            } else {
+                value[i] *= pow(0.001, 1.0 / (releaseBuffer[k] * getSampleRate()));
+                outputBuffer[k] = value[i];
+            }
+        }
+    }
+}
---
+++ src/Units/Dynamics/AHR.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Generator.h"
+
+namespace dsp {
+
+class AHR : public Generator {
+
+public:
+    AHR();
+    std::shared_ptr<InputParameter> getResetTrigger();
+    std::shared_ptr<InputParameter> getAttack();
+    std::shared_ptr<InputParameter> getHold();
+    std::shared_ptr<InputParameter> getRelease();
+
+protected:
+    void setNumChannelsNoLock(std::size_t numChannels) override;
+    void process() override;
+
+private:
+    static const std::size_t RESET_TRIGGER;
+    static const std::size_t ATTACK;
+    static const std::size_t HOLD;
+    static const std::size_t RELEASE;
+    std::vector<unsigned int> position;
+    std::vector<DSP_FLOAT> value;
+};
+
+}
