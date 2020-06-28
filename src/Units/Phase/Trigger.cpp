b84deacb9c1b1c3b257a73394a278cd802282a9f
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-06-27 19:17:37 -0700

    Switch order of inputs in Trigger

--- src/Units/Phase/Trigger.cpp
+++ src/Units/Phase/Trigger.cpp
@@ -1,19 +1,19 @@
 #include "Trigger.h"
 
-const std::size_t dsp::Trigger::INTERVAL = 0;
-const std::size_t dsp::Trigger::RESET_TRIGGER = 1;
+const std::size_t dsp::Trigger::RESET_TRIGGER = 0;
+const std::size_t dsp::Trigger::INTERVAL = 1;
 
 dsp::Trigger::Trigger() : Generator(Connection::Type::UNIPOLAR) {
-    pushInput(Connection::Type::SECONDS);
     pushInput(Connection::Type::BINARY);
-}
-
-std::shared_ptr<dsp::Unit::InputParameter> dsp::Trigger::getInterval() {
-    return getInput(INTERVAL);
+    pushInput(Connection::Type::SECONDS);
 }
 
 std::shared_ptr<dsp::Unit::InputParameter> dsp::Trigger::getResetTrigger() {
     return getInput(RESET_TRIGGER);
+}
+
+std::shared_ptr<dsp::Unit::InputParameter> dsp::Trigger::getInterval() {
+    return getInput(INTERVAL);
 }
 
 void dsp::Trigger::setNumChannelsNoLock(std::size_t numChannels) {
@@ -25,8 +25,8 @@ void dsp::Trigger::process() {
     Unit::process();
     for (std::size_t i = 0; i < getNumChannels(); i++) {
         std::vector<DSP_FLOAT> &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
-        std::vector<DSP_FLOAT> &intervalBuffer = getInterval()->getChannel(i)->getBuffer();
         std::vector<DSP_FLOAT> &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
+        std::vector<DSP_FLOAT> &intervalBuffer = getInterval()->getChannel(i)->getBuffer();
         for (unsigned int k = 0; k < getBufferSize(); k++) {
             if (resetTriggerBuffer[k]) {
                 phase[i] = 1.0;
--- src/Units/Phase/Trigger.h
+++ src/Units/Phase/Trigger.h
@@ -8,16 +8,16 @@ class Trigger : public Generator {
 
 public:
     Trigger();
-    std::shared_ptr<InputParameter> getInterval();
     std::shared_ptr<InputParameter> getResetTrigger();
+    std::shared_ptr<InputParameter> getInterval();
 
 protected:
     void setNumChannelsNoLock(std::size_t numChannels) override;
     void process() override;
 
 private:
-    static const std::size_t INTERVAL;
     static const std::size_t RESET_TRIGGER;
+    static const std::size_t INTERVAL;
     std::vector<DSP_FLOAT> phase;
 };
 
