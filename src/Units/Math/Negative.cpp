125ac4505b62e9e2fc161ea753fdf7537ea8fcd3
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-06 12:35:15 -0700

    Add type restriction to Negative

--- src/Units/Math/Negative.cpp
+++ src/Units/Math/Negative.cpp
@@ -1,6 +1,8 @@
 #include "Negative.h"
 
-dsp::Negative::Negative(Connection::Type type, Connection::Space space) : Processor(type, type, space) {}
+dsp::Negative::Negative(Connection::Type type, Connection::Space space) : Processor(type, type, space) {
+    assert(type != Connection::Type::BINARY);
+}
 
 void dsp::Negative::process() {
     Unit::process();
