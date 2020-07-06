4fdd53a0f050ed359913fab42ef596b43f1695e8
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-06 08:51:11 -0700

    Change BufferSize type from INTEGER to RATIO

--- src/Units/Variables/BufferSize.cpp
+++ src/Units/Variables/BufferSize.cpp
@@ -1,6 +1,6 @@
 #include "BufferSize.h"
 
-dsp::BufferSize::BufferSize() : Generator(Connection::Type::INTEGER) {}
+dsp::BufferSize::BufferSize() : Generator(Connection::Type::RATIO) {}
 
 void dsp::BufferSize::setBufferSizeNoLock(unsigned int bufferSize) {
     Unit::setBufferSizeNoLock(bufferSize);
