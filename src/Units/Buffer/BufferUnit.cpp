aa1278aa5cab4638aaf9b081a4cdee5aa4cdae16
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-12 01:55:55 -0700

    Add double buffering and space argument to BufferUnit

--- src/Units/Buffer/BufferUnit.cpp
+++ src/Units/Buffer/BufferUnit.cpp
@@ -1,7 +1,31 @@
 #include "BufferUnit.h"
 
-dsp::BufferUnit::BufferUnit(unsigned int bufferSize, Type type) : Consumer(type), writeIndex(0) {
-    buffer = std::make_shared<Buffer>(0, bufferSize, type);
+dsp::BufferUnit::BufferUnit(unsigned int externalBufferSize, Type type, Space space, DSP_FLOAT defaultValue)
+        : Consumer(type, space), mode(Mode::SINGLE_BUFFER), writeIndex(0) {
+    buffer = std::make_shared<Buffer>(0, externalBufferSize, type, space, defaultValue);
+    second = std::make_shared<Buffer>(0, externalBufferSize, type, space, defaultValue);
+}
+
+dsp::BufferUnit::Mode dsp::BufferUnit::getMode() const {
+    return mode;
+}
+
+void dsp::BufferUnit::setMode(Mode mode) {
+    lock();
+    this->mode = mode;
+    unlock();
+}
+
+unsigned int dsp::BufferUnit::getExternalBufferSize() const {
+    return externalBufferSize;
+}
+
+void dsp::BufferUnit::setExternalBufferSize(unsigned int externalBufferSize) {
+    lock();
+    this->externalBufferSize = externalBufferSize;
+    buffer->setBufferSize(externalBufferSize);
+    second->setBufferSize(externalBufferSize);
+    unlock();
 }
 
 std::shared_ptr<dsp::Buffer> dsp::BufferUnit::getBuffer() const {
@@ -11,26 +35,42 @@ std::shared_ptr<dsp::Buffer> dsp::BufferUnit::getBuffer() const {
 void dsp::BufferUnit::setNumChannelsNoLock(unsigned int numChannels) {
     Unit::setNumChannelsNoLock(numChannels);
     buffer->setNumChannels(numChannels);
+    second->setNumChannels(numChannels);
 }
 
 void dsp::BufferUnit::process() {
     Unit::process();
     buffer->lock();
-    if (buffer->getBufferSize() > 0) {
-        writeIndex %= buffer->getBufferSize();
-        unsigned int start = writeIndex;
+    if (externalBufferSize > 0) {
+        if (getInputSignal()->getSpace() == Space::FREQUENCY) {
+            assert(getBufferSize() == externalBufferSize);
+        }
+        if (writeIndex >= externalBufferSize || getBufferSize() == externalBufferSize) {
+            writeIndex = 0;
+        }
         for (unsigned int i = 0; i < getNumChannels(); i++) {
             std::vector<DSP_FLOAT> &inputBuffer = getInputSignal()->getChannel(i)->getBuffer();
-            unsigned int index = start;
+            unsigned int index = writeIndex;
             for (unsigned int k = 0; k < getBufferSize(); k++) {
-                buffer->getChannel(i)[index] = inputBuffer[k];
+                switch (mode) {
+                    case Mode::SINGLE_BUFFER: buffer->getChannel(i)[index] = inputBuffer[k]; break;
+                    case Mode::DOUBLE_BUFFER: second->getChannel(i)[index] = inputBuffer[k]; break;
+                }
                 index++;
-                if (index >= buffer->getBufferSize()) {
-                    index -= buffer->getBufferSize();
+                if (index >= externalBufferSize) {
+                    index = 0;
+                    if (mode == Mode::DOUBLE_BUFFER) {
+                        std::copy(second->getChannel(i).begin(),
+                                  second->getChannel(i).end(),
+                                  buffer->getChannel(i).begin());
+                    }
                 }
             }
         }
-        writeIndex += getBufferSize();
+        if (getBufferSize() != externalBufferSize) {
+            writeIndex += getBufferSize();
+            writeIndex %= externalBufferSize;
+        }
     }
     buffer->unlock();
 }
--- src/Units/Buffer/BufferUnit.h
+++ src/Units/Buffer/BufferUnit.h
@@ -8,7 +8,13 @@ namespace dsp {
 class BufferUnit : public Consumer {
 
 public:
-    BufferUnit(unsigned int bufferSize, Type type);
+    enum class Mode { SINGLE_BUFFER, DOUBLE_BUFFER };
+
+    BufferUnit(unsigned int externalBufferSize, Type type, Space space = Space::TIME, DSP_FLOAT defaultValue = 0.0);
+    Mode getMode() const;
+    void setMode(Mode mode);
+    unsigned int getExternalBufferSize() const;
+    void setExternalBufferSize(unsigned int externalBufferSize);
     std::shared_ptr<Buffer> getBuffer() const;
 
 protected:
@@ -16,7 +22,10 @@ protected:
     void process() override;
 
 private:
+    Mode mode;
+    unsigned int externalBufferSize;
     std::shared_ptr<Buffer> buffer;
+    std::shared_ptr<Buffer> second;
     unsigned int writeIndex;
 };
 
