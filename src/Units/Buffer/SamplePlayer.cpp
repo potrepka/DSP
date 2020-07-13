db5d4d99d7fc501d5837afef777acff999127216
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-13 06:17:09 -0700

    Remove const qualifiers and rearrange code

--- src/Units/Buffer/SamplePlayer.cpp
+++ src/Units/Buffer/SamplePlayer.cpp
@@ -63,15 +63,16 @@ void dsp::SamplePlayer::process() {
     Unit::process();
     if (sample != nullptr) {
         sample->lock();
-        if (sample->getNumChannels() > 0) {
+        unsigned int numChannels = sample->getNumChannels();
+        unsigned int channelSize = sample->getBufferSize();
+        if (numChannels > 0) {
             for (unsigned int i = 0; i < getNumChannels(); i++) {
-                std::vector<DSP_FLOAT> &channel = sample->getChannel(i % sample->getNumChannels());
-                const unsigned int size = static_cast<unsigned int>(channel.size());
                 switch (mode) {
-                    case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, size); break;
-                    case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, size); break;
+                    case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, channelSize); break;
+                    case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, channelSize); break;
                 }
-                if (size > 0) {
+                if (channelSize > 0) {
+                    std::vector<DSP_FLOAT> &channel = sample->getChannel(i % numChannels);
                     std::vector<DSP_FLOAT> &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
                     std::vector<DSP_FLOAT> &gateBuffer = getGate()->getChannel(i)->getBuffer();
                     std::vector<DSP_FLOAT> &startTimeBuffer = getStartTime()->getChannel(i)->getBuffer();
@@ -80,20 +81,20 @@ void dsp::SamplePlayer::process() {
                     std::vector<DSP_FLOAT> &currentTimeBuffer = getCurrentTime()->getChannel(i)->getBuffer();
                     for (unsigned int k = 0; k < getBufferSize(); k++) {
                         if (resetTriggerBuffer[k]) {
-                            readIndex[i] = wrap(startTimeBuffer[k] * getSampleRate(), 0.0, size);
+                            readIndex[i] = wrap(startTimeBuffer[k] * getSampleRate(), 0.0, channelSize);
                         }
                         if (gateBuffer[k]) {
-                            const unsigned int k1 = static_cast<int>(readIndex[i]);
-                            const unsigned int k0 = (k1 + size - 1) % size;
-                            const unsigned int k2 = (k1 + 1) % size;
-                            const unsigned int k3 = (k1 + 2) % size;
+                            unsigned int k1 = static_cast<unsigned int>(readIndex[i]);
+                            unsigned int k0 = (k1 + channelSize - 1) % channelSize;
+                            unsigned int k2 = (k1 + 1) % channelSize;
+                            unsigned int k3 = (k1 + 2) % channelSize;
                             std::vector<DSP_FLOAT> points{channel[k0], channel[k1], channel[k2], channel[k3]};
                             outputBuffer[k] = hermite(points, 1 + readIndex[i] - k1);
                             currentTimeBuffer[k] = readIndex[i] * getOneOverSampleRate();
                             readIndex[i] += speedBuffer[k];
                             switch (mode) {
-                                case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, size); break;
-                                case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, size); break;
+                                case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, channelSize); break;
+                                case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, channelSize); break;
                             }
                         }
                     }
