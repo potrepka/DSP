8ed9496928655a9b27f7b9b896ec53f1991c13e4
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-13 00:50:10 -0700

    Change type of BufferSize unit to INTEGER

--- src/Units/Variables/BufferSize.cpp
+++ src/Units/Variables/BufferSize.cpp
@@ -1,6 +1,6 @@
 #include "BufferSize.h"
 
-dsp::BufferSize::BufferSize(Space space) : Generator(Type::RATIO, space) {}
+dsp::BufferSize::BufferSize(Space space) : Generator(Type::INTEGER, space) {}
 
 void dsp::BufferSize::setBufferSizeNoLock(unsigned int bufferSize) {
     Unit::setBufferSizeNoLock(bufferSize);
