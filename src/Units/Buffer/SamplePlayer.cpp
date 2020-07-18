36135b1926d99d318e4bdd57cc68b1a202bf26aa
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-17 22:43:17 -0700

    Use BufferCollection in SamplePlayer

--- src/Units/Buffer/SamplePlayer.cpp
+++ src/Units/Buffer/SamplePlayer.cpp
@@ -19,14 +19,36 @@ void dsp::SamplePlayer::setMode(Mode mode) {
     unlock();
 }
 
-std::shared_ptr<dsp::Buffer> dsp::SamplePlayer::getSample() const {
-    return sample;
+unsigned int dsp::SamplePlayer::getNumSamples() const {
+    return getNumBuffers();
 }
 
-void dsp::SamplePlayer::setSample(std::shared_ptr<Buffer> sample) {
-    lock();
-    this->sample = sample;
-    unlock();
+std::shared_ptr<dsp::Buffer> dsp::SamplePlayer::getSample(unsigned int index) const {
+    return getBuffer(index);
+}
+
+std::vector<std::shared_ptr<dsp::Buffer>> dsp::SamplePlayer::getSamples(unsigned int begin, unsigned int end) const {
+    return getBuffers(begin, end);
+}
+
+void dsp::SamplePlayer::pushSample(std::shared_ptr<Buffer> sample) {
+    pushBuffer(sample);
+}
+
+void dsp::SamplePlayer::pushSamples(std::vector<std::shared_ptr<Buffer>> samples) {
+    pushBuffers(samples);
+}
+
+void dsp::SamplePlayer::replaceSample(std::shared_ptr<Buffer> sample, std::shared_ptr<Buffer> replacement) {
+    replaceBuffer(sample, replacement);
+}
+
+void dsp::SamplePlayer::removeSample(std::shared_ptr<Buffer> sample) {
+    removeBuffer(sample);
+}
+
+void dsp::SamplePlayer::removeSamples(std::vector<std::shared_ptr<Buffer>> samples) {
+    removeBuffers(samples);
 }
 
 std::shared_ptr<dsp::InputParameter> dsp::SamplePlayer::getResetTrigger() const {
@@ -45,6 +67,10 @@ std::shared_ptr<dsp::InputParameter> dsp::SamplePlayer::getSpeed() const {
     return speed;
 }
 
+std::shared_ptr<dsp::InputParameter> dsp::SamplePlayer::getSampleIndex() const {
+    return sampleIndex;
+}
+
 std::shared_ptr<dsp::OutputParameter> dsp::SamplePlayer::getCurrentTime() const {
     return currentTime;
 }
@@ -56,31 +82,37 @@ void dsp::SamplePlayer::setNumChannelsNoLock(unsigned int numChannels) {
 
 void dsp::SamplePlayer::process() {
     Unit::process();
-    if (sample != nullptr) {
-        sample->lock();
-        unsigned int numChannels = sample->getNumChannels();
-        unsigned int channelSize = sample->getBufferSize();
-        if (numChannels > 0) {
-            for (unsigned int i = 0; i < getNumChannels(); i++) {
-                switch (mode) {
-                    case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, channelSize); break;
-                    case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, channelSize); break;
+    if (collection.size() > 0) {
+        for (const auto &buffer : collection) {
+            if (buffer != nullptr) {
+                buffer->lock();
+            }
+        }
+        for (unsigned int i = 0; i < getNumChannels(); i++) {
+            std::vector<DSP_FLOAT> &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &gateBuffer = getGate()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &offsetTimeBuffer = getOffsetTime()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &speedBuffer = getSpeed()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &sampleIndexBuffer = getSampleIndex()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
+            std::vector<DSP_FLOAT> &currentTimeBuffer = getCurrentTime()->getChannel(i)->getBuffer();
+            for (unsigned int k = 0; k < getBufferSize(); k++) {
+                if (resetTriggerBuffer[k]) {
+                    readIndex[i] = 0.0;
                 }
-                if (channelSize > 0) {
-                    std::vector<DSP_FLOAT> &channel = sample->getChannel(i % numChannels);
-                    std::vector<DSP_FLOAT> &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
-                    std::vector<DSP_FLOAT> &gateBuffer = getGate()->getChannel(i)->getBuffer();
-                    std::vector<DSP_FLOAT> &offsetTimeBuffer = getOffsetTime()->getChannel(i)->getBuffer();
-                    std::vector<DSP_FLOAT> &speedBuffer = getSpeed()->getChannel(i)->getBuffer();
-                    std::vector<DSP_FLOAT> &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
-                    std::vector<DSP_FLOAT> &currentTimeBuffer = getCurrentTime()->getChannel(i)->getBuffer();
-                    for (unsigned int k = 0; k < getBufferSize(); k++) {
-                        if (resetTriggerBuffer[k]) {
-                            readIndex[i] = 0.0;
-                        }
+                DSP_FLOAT p = sampleIndexBuffer[k];
+                if (p >= 0 && p < collection.size() && collection[p] != nullptr) {
+                    unsigned int numChannels = collection[p]->getNumChannels();
+                    unsigned int channelSize = collection[p]->getBufferSize();
+                    if (numChannels > 0 && channelSize > 0) {
+                        std::vector<DSP_FLOAT> &channel = collection[p]->getChannel(i % numChannels);
                         if (gateBuffer[k]) {
-                            DSP_FLOAT index =
-                                    wrap(readIndex[i] + offsetTimeBuffer[k] * getSampleRate(), 0.0, channelSize);
+                            DSP_FLOAT offset = offsetTimeBuffer[k] * getSampleRate();
+                            DSP_FLOAT index;
+                            switch (mode) {
+                                case Mode::ONE_SHOT: index = clip(readIndex[i] + offset, 0.0, channelSize); break;
+                                case Mode::LOOP: index = wrap(readIndex[i] + offset, 0.0, channelSize); break;
+                            }
                             unsigned int k1 = static_cast<unsigned int>(index);
                             unsigned int k0 = (k1 + channelSize - 1) % channelSize;
                             unsigned int k2 = (k1 + 1) % channelSize;
@@ -89,15 +121,17 @@ void dsp::SamplePlayer::process() {
                             outputBuffer[k] = hermite(points, 1.0 + readIndex[i] - k1);
                             currentTimeBuffer[k] = readIndex[i] * getOneOverSampleRate();
                             readIndex[i] += speedBuffer[k];
-                            switch (mode) {
-                                case Mode::ONE_SHOT: readIndex[i] = clip(readIndex[i], 0.0, channelSize); break;
-                                case Mode::LOOP: readIndex[i] = wrap(readIndex[i], 0.0, channelSize); break;
-                            }
                         }
+                    } else if (gateBuffer[k]) {
+                        outputBuffer[k] = collection[p]->getDefaultValue();
                     }
                 }
             }
         }
+        for (const auto &buffer : collection) {
+            if (buffer != nullptr) {
+                buffer->unlock();
+            }
+        }
     }
-    sample->unlock();
 }
--- src/Units/Buffer/SamplePlayer.h
+++ src/Units/Buffer/SamplePlayer.h
@@ -5,7 +5,7 @@
 
 namespace dsp {
 
-class SamplePlayer : public Generator {
+class SamplePlayer : public Generator, public BufferCollection {
 
 public:
     enum class Mode { ONE_SHOT, LOOP };
@@ -14,12 +14,19 @@ public:
 
     Mode getMode() const;
     void setMode(Mode mode);
-    std::shared_ptr<Buffer> getSample() const;
-    void setSample(std::shared_ptr<Buffer> sample);
+    unsigned int getNumSamples() const;
+    std::shared_ptr<Buffer> getSample(unsigned int index) const;
+    std::vector<std::shared_ptr<Buffer>> getSamples(unsigned int begin, unsigned int end) const;
+    void pushSample(std::shared_ptr<Buffer> sample);
+    void pushSamples(std::vector<std::shared_ptr<Buffer>> samples);
+    void replaceSample(std::shared_ptr<Buffer> sample, std::shared_ptr<Buffer> replacement);
+    void removeSample(std::shared_ptr<Buffer> sample);
+    void removeSamples(std::vector<std::shared_ptr<Buffer>> samples);
     std::shared_ptr<InputParameter> getResetTrigger() const;
     std::shared_ptr<InputParameter> getGate() const;
     std::shared_ptr<InputParameter> getOffsetTime() const;
     std::shared_ptr<InputParameter> getSpeed() const;
+    std::shared_ptr<InputParameter> getSampleIndex() const;
     std::shared_ptr<OutputParameter> getCurrentTime() const;
 
 protected:
@@ -31,11 +38,11 @@ private:
     const std::shared_ptr<InputParameter> gate;
     const std::shared_ptr<InputParameter> offsetTime;
     const std::shared_ptr<InputParameter> speed;
+    const std::shared_ptr<InputParameter> sampleIndex;
 
     const std::shared_ptr<OutputParameter> currentTime;
 
     Mode mode;
-    std::shared_ptr<Buffer> sample;
     std::vector<DSP_FLOAT> readIndex;
 };
 
