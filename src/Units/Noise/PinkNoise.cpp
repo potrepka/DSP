5b14b99b07b359b3d731ff99f78ea87bbd785c91
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-20 08:32:53 -0700

    Vectorize pink noise

--- src/Units/Noise/PinkNoise.cpp
+++ src/Units/Noise/PinkNoise.cpp
@@ -1,36 +1,90 @@
 #include "PinkNoise.h"
 
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_0 = 0.99886;
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_1 = 0.99332;
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_2 = 0.96900;
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_3 = 0.86650;
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_4 = 0.55000;
+const dsp::Sample dsp::PinkNoise::VALUE_COEFFICIENT_5 = -0.7616;
+
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_0 = 0.0555179;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_1 = 0.0750759;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_2 = 0.1538520;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_3 = 0.3104856;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_4 = 0.5329522;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_5 = -0.016898;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_6 = 0.5362;
+const dsp::Sample dsp::PinkNoise::NOISE_COEFFICIENT_7 = 0.115926;
+
 dsp::PinkNoise::PinkNoise()
         : Generator(Type::BIPOLAR)
-        , seed(1) {}
+        , seed(1) {
+#if DSP_USE_VC
+    valueCoefficients[0] = VALUE_COEFFICIENT_0;
+    valueCoefficients[1] = VALUE_COEFFICIENT_1;
+    valueCoefficients[2] = VALUE_COEFFICIENT_2;
+    valueCoefficients[3] = VALUE_COEFFICIENT_3;
+    valueCoefficients[4] = VALUE_COEFFICIENT_4;
+    valueCoefficients[5] = VALUE_COEFFICIENT_5;
+    valueCoefficients[6] = 0.0;
+    valueCoefficients[7] = 1.0;
+
+    noiseCoefficients[0] = NOISE_COEFFICIENT_0;
+    noiseCoefficients[1] = NOISE_COEFFICIENT_1;
+    noiseCoefficients[2] = NOISE_COEFFICIENT_2;
+    noiseCoefficients[3] = NOISE_COEFFICIENT_3;
+    noiseCoefficients[4] = NOISE_COEFFICIENT_4;
+    noiseCoefficients[5] = NOISE_COEFFICIENT_5;
+    noiseCoefficients[6] = NOISE_COEFFICIENT_6;
+    noiseCoefficients[7] = 0.0;
+#endif
+}
 
 void dsp::PinkNoise::setNumChannelsNoLock(unsigned int numChannels) {
     Unit::setNumChannelsNoLock(numChannels);
-    values.resize(numChannels, Array(7, 0.0));
+#if DSP_USE_VC
+    values.resize(numChannels, Vc::Memory<Vector, 8>() = 0.0);
+#else
+    values.resize(numChannels, Array(8, 0.0));
+#endif
 }
 
 void dsp::PinkNoise::process() {
     Unit::process();
     for (unsigned int i = 0; i < getNumChannels(); ++i) {
         Array &outputBuffer = getOutputSignal()->getChannel(i)->getBuffer();
-        Sample &b0 = values[i][0];
-        Sample &b1 = values[i][1];
-        Sample &b2 = values[i][2];
-        Sample &b3 = values[i][3];
-        Sample &b4 = values[i][4];
-        Sample &b5 = values[i][5];
-        Sample &b6 = values[i][6];
+#if DSP_USE_VC
+        Vc::Memory<Vector, 8> &b = values[i];
+#else
+        Array &b = values[i];
+#endif
         for (unsigned int k = 0; k < getBufferSize(); ++k) {
             seed *= 16807;
             Sample white = static_cast<Sample>(seed) * 4.65661287e-10;
-            b0 = 0.99886 * b0 + white * 0.0555179;
-            b1 = 0.99332 * b1 + white * 0.0750759;
-            b2 = 0.96900 * b2 + white * 0.1538520;
-            b3 = 0.86650 * b3 + white * 0.3104856;
-            b4 = 0.55000 * b4 + white * 0.5329522;
-            b5 = -0.7616 * b5 - white * 0.0168980;
-            outputBuffer[k] = 0.125 * (b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362);
-            b6 = white * 0.115926;
+#if DSP_USE_VC
+            Vector sum = 0.0;
+            for (std::size_t m = 0; m < b.vectorsCount(); ++m) {
+                const Vector value = valueCoefficients.vector(m);
+                const Vector noise = noiseCoefficients.vector(m);
+                b.vector(m) = b.vector(m) * value + white * noise;
+                sum += b.vector(m);
+            }
+            outputBuffer[k] = 0.125 * sum.sum();
+#else
+            b[0] = b[0] * VALUE_COEFFICIENT_0 + white * NOISE_COEFFICIENT_0;
+            b[1] = b[1] * VALUE_COEFFICIENT_1 + white * NOISE_COEFFICIENT_1;
+            b[2] = b[2] * VALUE_COEFFICIENT_2 + white * NOISE_COEFFICIENT_2;
+            b[3] = b[3] * VALUE_COEFFICIENT_3 + white * NOISE_COEFFICIENT_3;
+            b[4] = b[4] * VALUE_COEFFICIENT_4 + white * NOISE_COEFFICIENT_4;
+            b[5] = b[5] * VALUE_COEFFICIENT_5 + white * NOISE_COEFFICIENT_5;
+            b[6] = white * NOISE_COEFFICIENT_6;
+            Sample sum = 0.0;
+            for (const auto &value : b) {
+                sum += value;
+            }
+            outputBuffer[k] = 0.125 * sum;
+#endif
+            b[7] = white * NOISE_COEFFICIENT_7;
         }
     }
 }
--- src/Units/Noise/PinkNoise.h
+++ src/Units/Noise/PinkNoise.h
@@ -14,8 +14,30 @@ protected:
     void process() override;
 
 private:
+    static const Sample VALUE_COEFFICIENT_0;
+    static const Sample VALUE_COEFFICIENT_1;
+    static const Sample VALUE_COEFFICIENT_2;
+    static const Sample VALUE_COEFFICIENT_3;
+    static const Sample VALUE_COEFFICIENT_4;
+    static const Sample VALUE_COEFFICIENT_5;
+
+    static const Sample NOISE_COEFFICIENT_0;
+    static const Sample NOISE_COEFFICIENT_1;
+    static const Sample NOISE_COEFFICIENT_2;
+    static const Sample NOISE_COEFFICIENT_3;
+    static const Sample NOISE_COEFFICIENT_4;
+    static const Sample NOISE_COEFFICIENT_5;
+    static const Sample NOISE_COEFFICIENT_6;
+    static const Sample NOISE_COEFFICIENT_7;
+
     int seed;
+#if DSP_USE_VC
+    std::vector<Vc::Memory<Vector, 8>> values;
+    Vc::Memory<Vector, 8> valueCoefficients;
+    Vc::Memory<Vector, 8> noiseCoefficients;
+#else
     std::vector<Array> values;
+#endif
 };
 
 } // namespace dsp
