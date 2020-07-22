9d0db38f9de56c8920e2da392837c98bed472a3f
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-22 02:47:47 -0700

    Use vector constant in HardClip

--- src/Units/Dynamics/HardClip.cpp
+++ src/Units/Dynamics/HardClip.cpp
@@ -6,7 +6,7 @@ dsp::HardClip::HardClip(Space space)
 void dsp::HardClip::process() {
     Unit::process();
 #if DSP_USE_VC
-    transform([](Vector x) { return clip(x, -1.0, 1.0); });
+    transform([](Vector x) { return clip(x, -Vector::One(), Vector::One()); });
 #else
     transform([](Sample x) { return clip(x, -1.0, 1.0); });
 #endif
