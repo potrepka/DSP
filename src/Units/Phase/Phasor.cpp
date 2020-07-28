c7d7290377d3b520d62e8f02419e199dac10a2f2
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-07-27 23:56:23 -0700

    Add unbounded mode to Phasor

--- src/Units/Phase/Phasor.cpp
+++ src/Units/Phase/Phasor.cpp
@@ -3,7 +3,18 @@
 dsp::Phasor::Phasor()
         : Generator(Type::UNIPOLAR)
         , resetTrigger(pushInput(Type::BINARY))
-        , frequency(pushInput(Type::HERTZ)) {}
+        , frequency(pushInput(Type::HERTZ))
+        , mode(Mode::WRAPPED) {}
+
+dsp::Phasor::Mode dsp::Phasor::getMode() {
+    return mode;
+}
+
+void dsp::Phasor::setMode(Mode mode) {
+    lock();
+    this->mode = mode;
+    unlock();
+}
 
 std::shared_ptr<dsp::InputParameter> dsp::Phasor::getResetTrigger() const {
     return resetTrigger;
@@ -20,6 +31,7 @@ void dsp::Phasor::setNumChannelsNoLock(unsigned int numChannels) {
 
 void dsp::Phasor::process() {
     Unit::process();
+    const bool wrapped = mode == Mode::WRAPPED;
     for (unsigned int i = 0; i < getNumChannels(); ++i) {
         Array &resetTriggerBuffer = getResetTrigger()->getChannel(i)->getBuffer();
         Array &frequencyBuffer = getFrequency()->getChannel(i)->getBuffer();
@@ -29,7 +41,10 @@ void dsp::Phasor::process() {
                 phase[i] = 0.0;
             }
             outputBuffer[k] = phase[i];
-            phase[i] = wrap(phase[i] + frequencyBuffer[k] * getOneOverSampleRate(), 1.0);
+            phase[i] += frequencyBuffer[k] * getOneOverSampleRate();
+            if (wrapped) {
+                phase[i] = wrap(phase[i], 1.0);
+            }
         }
     }
 }
--- src/Units/Phase/Phasor.h
+++ src/Units/Phase/Phasor.h
@@ -7,8 +7,12 @@ namespace dsp {
 class Phasor : public Generator {
 
 public:
+    enum class Mode { WRAPPED, UNBOUNDED };
+
     Phasor();
 
+    Mode getMode();
+    void setMode(Mode mode);
     std::shared_ptr<InputParameter> getResetTrigger() const;
     std::shared_ptr<InputParameter> getFrequency() const;
 
@@ -20,6 +24,7 @@ private:
     const std::shared_ptr<InputParameter> resetTrigger;
     const std::shared_ptr<InputParameter> frequency;
 
+    Mode mode;
     Array phase;
 };
 
