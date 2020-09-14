5d89bb5e811d54828348ffe8fe9542139f1f1c5a
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-09-13 23:11:59 -0400

    Modify VariableDelay

--- src/Nodes/Delays/VariableDelay.cpp
+++ src/Nodes/Delays/VariableDelay.cpp
@@ -3,11 +3,14 @@
 dsp::VariableDelay::VariableDelay(Type type)
         : Transformer(type, type)
         , maxDelayTime(0.0)
+        , buffer(std::make_shared<Buffer>(type))
         , resetTrigger(std::make_shared<Input>(Type::INTEGER))
         , delayTime(std::make_shared<Input>(Type::SECONDS))
         , decayTime(std::make_shared<Input>(Type::SECONDS))
-        , indexState(0)
-        , buffer(std::make_shared<Buffer>(type)) {
+        , feedbackSource(std::make_shared<Output>(type))
+        , feedbackSink(std::make_shared<Input>(type))
+        , feedbackProcessor(std::make_shared<Node>())
+        , indexState(0) {
     getInputs().push_back(resetTrigger);
     getInputs().push_back(delayTime);
     getInputs().push_back(decayTime);
@@ -38,39 +41,66 @@ std::shared_ptr<dsp::Input> dsp::VariableDelay::getDecayTime() const {
     return decayTime;
 }
 
+std::shared_ptr<dsp::Output> dsp::VariableDelay::getFeedbackSource() const {
+    return feedbackSource;
+}
+
+std::shared_ptr<dsp::Input> dsp::VariableDelay::getFeedbackSink() const {
+    return feedbackSink;
+}
+
+std::shared_ptr<dsp::Node> dsp::VariableDelay::getFeedbackProcessor() const {
+    return feedbackProcessor;
+}
+
+void dsp::VariableDelay::setNumInputChannelsNoLock(size_t numChannels) {
+    Node::setNumInputChannelsNoLock(numChannels);
+}
+
 void dsp::VariableDelay::setNumOutputChannelsNoLock(size_t numChannels) {
     Node::setNumOutputChannelsNoLock(numChannels);
     buffer->setNumChannels(numChannels);
+    feedbackSource->setNumChannels(numChannels);
+    feedbackSink->setNumChannels(numChannels);
 }
 
 void dsp::VariableDelay::setSampleRateNoLock(double sampleRate) {
     Node::setSampleRateNoLock(sampleRate);
     indexState = 0;
     buffer->setNumSamples(getDelayBufferSize());
+    feedbackSource->setNumSamples(1);
+    feedbackSink->setNumSamples(1);
+    feedbackProcessor->setNumSamples(1);
 }
 
 void dsp::VariableDelay::processNoLock() {
+    feedbackSource->lock();
+    feedbackSink->lock();
+    feedbackProcessor->lock();
     if (buffer->getNumSamples() > 0) {
-        for (size_t channel = 0; channel < getNumChannels(); ++channel) {
-            Sample *inputChannel = getInput()->getWrapper().getChannelPointer(channel);
-            Sample *resetTriggerChannel = getResetTrigger()->getWrapper().getChannelPointer(channel);
-            Sample *delayTimeChannel = getDelayTime()->getWrapper().getChannelPointer(channel);
-            Sample *decayTimeChannel = getDecayTime()->getWrapper().getChannelPointer(channel);
-            Sample *outputChannel = getOutput()->getWrapper().getChannelPointer(channel);
-            Sample *bufferChannel = buffer->getWrapper().getChannelPointer(channel);
-            size_t writeIndex = indexState;
-            for (size_t sample = 0; sample < getNumSamples(); ++sample) {
+        std::vector<Sample> delayTimeClipped(getNumChannels());
+        size_t writeIndex = indexState;
+        for (size_t sample = 0; sample < getNumSamples(); ++sample) {
+            for (size_t channel = 0; channel < getNumChannels(); ++channel) {
+                Sample *bufferChannel = buffer->getWrapper().getChannelPointer(channel);
+                Sample *inputChannel = getInput()->getWrapper().getChannelPointer(channel);
+                Sample *resetTriggerChannel = getResetTrigger()->getWrapper().getChannelPointer(channel);
+                Sample *delayTimeChannel = getDelayTime()->getWrapper().getChannelPointer(channel);
+                Sample *feedbackSourceChannel = getFeedbackSource()->getWrapper().getChannelPointer(channel);
+                Sample *outputChannel = getOutput()->getWrapper().getChannelPointer(channel);
+                // RESET
                 if (resetTriggerChannel[sample]) {
                     buffer->getWrapper().getSingleChannel(channel).clear();
                 }
-                // WRITING
+                // WRITE
                 bufferChannel[writeIndex] = inputChannel[sample];
-                // READING
-                Sample delayTimeClipped = clip(delayTimeChannel[sample], 0.0, maxDelayTime);
-                if (delayTimeClipped < getOneOverSampleRate()) {
-                    delayTimeClipped = 0.0;
+                // CLIP DELAY TIME
+                delayTimeClipped[channel] = clip(delayTimeChannel[sample], 0.0, maxDelayTime);
+                if (delayTimeClipped[channel] < getOneOverSampleRate()) {
+                    delayTimeClipped[channel] = 0.0;
                 }
-                Sample readIndex = writeIndex - delayTimeClipped * getSampleRate();
+                // READ
+                Sample readIndex = writeIndex - delayTimeClipped[channel] * getSampleRate();
                 if (readIndex < 0.0) {
                     readIndex += buffer->getNumSamples();
                 }
@@ -78,21 +108,35 @@ void dsp::VariableDelay::processNoLock() {
                 if (std::isinf(outputChannel[sample]) || std::isnan(outputChannel[sample])) {
                     outputChannel[sample] = 0.0;
                 }
-                // FEEDBACK
-                if (delayTimeClipped >= getOneOverSampleRate()) {
-                    Sample feedback = pow(0.001, delayTimeClipped / decayTimeChannel[sample]);
-                    bufferChannel[writeIndex] += outputChannel[sample] * feedback;
+                *feedbackSourceChannel = outputChannel[sample];
+            }
+            // PROCESS FEEDBACK
+            if (feedbackProcessor->isActive()) {
+                for (const auto &child : feedbackProcessor->getChildren()) {
+                    child->process();
                 }
-                // INCREMENT
-                ++writeIndex;
-                if (writeIndex >= buffer->getNumSamples()) {
-                    writeIndex = 0;
-                }
+            }
+            feedbackSink->processNoLock();
+            // ADD FEEDBACK
+            for (size_t channel = 0; channel < getNumChannels(); ++channel) {
+                Sample *bufferChannel = buffer->getWrapper().getChannelPointer(channel);
+                Sample *decayTimeChannel = getDecayTime()->getWrapper().getChannelPointer(channel);
+                Sample *feedbackSinkChannel = getFeedbackSink()->getWrapper().getChannelPointer(channel);
+                Sample feedbackAmount = pow(0.001, delayTimeClipped[channel] / decayTimeChannel[sample]);
+                bufferChannel[writeIndex] += *feedbackSinkChannel * feedbackAmount;
+            }
+            // INCREMENT INDEX
+            ++writeIndex;
+            if (writeIndex >= buffer->getNumSamples()) {
+                writeIndex = 0;
             }
         }
         indexState += getNumSamples();
         indexState %= buffer->getNumSamples();
     }
+    feedbackProcessor->unlock();
+    feedbackSink->unlock();
+    feedbackSource->unlock();
 }
 
 size_t dsp::VariableDelay::getDelayBufferSize() {
--- src/Nodes/Delays/VariableDelay.h
+++ src/Nodes/Delays/VariableDelay.h
@@ -15,19 +15,27 @@ public:
     std::shared_ptr<Input> getResetTrigger() const;
     std::shared_ptr<Input> getDelayTime() const;
     std::shared_ptr<Input> getDecayTime() const;
+    std::shared_ptr<Output> getFeedbackSource() const;
+    std::shared_ptr<Input> getFeedbackSink() const;
+
+    std::shared_ptr<Node> getFeedbackProcessor() const;
 
 protected:
+    void setNumInputChannelsNoLock(size_t numChannels) override;
     void setNumOutputChannelsNoLock(size_t numChannels) override;
     void setSampleRateNoLock(double sampleRate) override;
     void processNoLock() override;
 
 private:
     Sample maxDelayTime;
+    const std::shared_ptr<Buffer> buffer;
     const std::shared_ptr<Input> resetTrigger;
     const std::shared_ptr<Input> delayTime;
     const std::shared_ptr<Input> decayTime;
+    const std::shared_ptr<Output> feedbackSource;
+    const std::shared_ptr<Input> feedbackSink;
+    const std::shared_ptr<Node> feedbackProcessor;
     size_t indexState;
-    std::shared_ptr<Buffer> buffer;
 
     size_t getDelayBufferSize();
 };
