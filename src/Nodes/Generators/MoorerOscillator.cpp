68382b26e73a9d909e15f6923e6ff9068192054c
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-09-30 03:38:07 -0400

    Fix MoorerOscillator intensity and overtones parameter calculations

--- src/Nodes/Generators/MoorerOscillator.cpp
+++ src/Nodes/Generators/MoorerOscillator.cpp
@@ -5,7 +5,7 @@ dsp::MoorerOscillator::MoorerOscillator()
         , mode(Mode::ONE_SIDED)
         , phase(std::make_shared<Input>(Type::RATIO))
         , modulationIndex(std::make_shared<Input>(Type::RATIO))
-        , intensity(std::make_shared<Input>(Type::LOGARITHMIC))
+        , intensity(std::make_shared<Input>(Type::RATIO))
         , overtones(std::make_shared<Input>(Type::INTEGER)) {
     getInputs().push_back(phase);
     getInputs().push_back(modulationIndex);
@@ -47,29 +47,35 @@ void dsp::MoorerOscillator::processNoLock() {
         Sample *overtonesChannel = getOvertones()->getWrapper().getChannelPointer(channel);
         Sample *outputChannel = getOutput()->getWrapper().getChannelPointer(channel);
         for (int sample = 0; sample < getNumSamples(); ++sample) {
-            Sample t = TAU * phaseChannel[sample];
-            Sample s = modulationIndexChannel[sample] * t;
-            Sample a = exp2(-1.0 / intensityChannel[sample]);
-            Sample n = overtonesChannel[sample];
-            Sample aa = a * a;
-            Sample a1 = a;
-            for (size_t j = 0; j < static_cast<size_t>(n); ++j) {
-                a1 *= a;
+            Sample a = clip(intensityChannel[sample], 0.0, 1.0);
+            if (a == 1.0) {
+                outputChannel[sample] = 0.0;
+            } else {
+                Sample t = TAU * phaseChannel[sample];
+                Sample s = modulationIndexChannel[sample] * t;
+                Sample n = overtonesChannel[sample] >= 0.0 ? overtonesChannel[sample] : 0.0;
+                size_t nFloor = static_cast<size_t>(n);
+                Sample aa = a * a;
+                Sample a1 = a;
+                for (size_t j = 0; j < nFloor / 2; ++j) {
+                    a1 *= aa;
+                }
+                a1 *= nFloor % 2 ? a : 1;
+                Sample scale;
+                Sample numerator;
+                switch (mode) {
+                    case Mode::ONE_SIDED:
+                        scale = (a - 1.0) / (a1 - 1.0);
+                        numerator = sin(t) - a * sin(t - s) - a1 * (sin(t + (n + 1.0) * s) - a * sin(t + n * s));
+                        break;
+                    case Mode::TWO_SIDED:
+                        scale = (a - 1.0) / (a1 + a1 - a - 1.0);
+                        numerator = sin(t) * (1.0 - aa - (a1 + a1) * (cos((n + 1.0) * s) - a * cos(n * s)));
+                        break;
+                }
+                Sample denominator = 1.0 + aa - (a + a) * cos(s);
+                outputChannel[sample] = scale * numerator / denominator;
             }
-            Sample scale;
-            Sample numerator;
-            switch (mode) {
-                case Mode::ONE_SIDED:
-                    scale = (a - 1.0) / (a1 - 1.0);
-                    numerator = sin(t) - a * sin(t - s) - a1 * (sin(t + (n + 1.0) * s) - a * sin(t + n * s));
-                    break;
-                case Mode::TWO_SIDED:
-                    scale = (a - 1.0) / (a1 + a1 - a - 1.0);
-                    numerator = sin(t) * (1.0 - aa - (a1 + a1) * (cos((n + 1.0) * s) - a * cos(n * s)));
-                    break;
-            }
-            Sample denominator = 1.0 + aa - (a + a) * cos(s);
-            outputChannel[sample] = scale * numerator / denominator;
         }
     }
 }
