96795327a6bd84f88a5409536170636f6fd1d801
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-10-23 00:19:27 -0400

    Improve Clipper

--- src/Nodes/Dynamics/Clipper.cpp
+++ src/Nodes/Dynamics/Clipper.cpp
@@ -1,26 +1,54 @@
 #include "Clipper.h"
 
-dsp::Clipper::Clipper(Space space)
-        : Transformer(Type::RATIO, Type::RATIO, space)
-        , mode(Mode::CLIP) {}
+dsp::Clipper::Clipper(Type type, Space space)
+        : Transformer(type, type, space)
+        , mode(Mode::CLIP)
+        , minimum(std::make_shared<Input>(type, space, -1.0))
+        , maximum(std::make_shared<Input>(type, space, 1.0)) {
+    getInputs().push_back(minimum);
+    getInputs().push_back(maximum);
+}
 
 dsp::Clipper::Mode dsp::Clipper::getMode() const {
     return mode;
 }
 
 void dsp::Clipper::setMode(Mode mode) {
+    lock();
     this->mode = mode;
+    unlock();
+}
+
+std::shared_ptr<dsp::Input> dsp::Clipper::getMinimum() const {
+    return minimum;
+}
+
+std::shared_ptr<dsp::Input> dsp::Clipper::getMaximum() const {
+    return maximum;
 }
 
 void dsp::Clipper::processNoLock() {
-    transform([this](Sample x) {
-        switch (mode) {
-            case Mode::CLIP: return clip(x, -1.0, 1.0);
-            case Mode::WRAP: return wrap(1.0 + x, 2.0) - 1.0;
-            case Mode::MIRROR: {
-                Sample onePlusX = 1.0 + x;
-                return (wrap(onePlusX, 2.0) - 1.0) * (wrap(onePlusX, 4.0) < 2.0 ? 1.0 : -1.0);
+    for (size_t channel = 0; channel < getNumChannels(); ++channel) {
+        Sample *inputChannel = getInput()->getWrapper().getChannelPointer(channel);
+        Sample *minimumChannel = getMinimum()->getWrapper().getChannelPointer(channel);
+        Sample *maximumChannel = getMaximum()->getWrapper().getChannelPointer(channel);
+        Sample *outputChannel = getOutput()->getWrapper().getChannelPointer(channel);
+        for (size_t sample = 0; sample < getNumSamples(); ++sample) {
+            Sample &input = inputChannel[sample];
+            Sample &minimum = minimumChannel[sample];
+            Sample &maximum = maximumChannel[sample];
+            Sample &output = outputChannel[sample];
+            switch (mode) {
+                case Mode::CLIP: output = clip(input, minimum, maximum); break;
+                case Mode::WRAP: output = wrap(input - minimum, maximum - minimum) + minimum; break;
+                case Mode::MIRROR: {
+                    Sample offset = input - minimum;
+                    Sample range = maximum - minimum;
+                    Sample wrapped = wrap(offset, range);
+                    Sample flipped = wrap(offset, 2.0 * range) < range ? wrapped : range - wrapped;
+                    output = flipped + minimum;
+                } break;
             }
         }
-    });
+    }
 }
--- src/Nodes/Dynamics/Clipper.h
+++ src/Nodes/Dynamics/Clipper.h
@@ -9,16 +9,21 @@ class Clipper : public Transformer {
 public:
     enum class Mode { CLIP, WRAP, MIRROR };
 
-    Clipper(Space space = Space::TIME);
+    Clipper(Type type = Type::RATIO, Space space = Space::TIME);
 
     Mode getMode() const;
     void setMode(Mode mode);
+
+    std::shared_ptr<Input> getMinimum() const;
+    std::shared_ptr<Input> getMaximum() const;
 
 protected:
     void processNoLock() override;
 
 private:
     Mode mode;
+    std::shared_ptr<Input> minimum;
+    std::shared_ptr<Input> maximum;
 };
 
 } // namespace dsp
