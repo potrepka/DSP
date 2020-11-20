eb2bc7a0db0e23febdccaf300307fb9cb1eb3d43
Nathaniel Potrepka <nspotrepka@gmail.com>
2020-11-20 02:03:37 -0500

    Change function to function pointer

--- src/Nodes/Generators/FunctionOscillator.cpp
+++ src/Nodes/Generators/FunctionOscillator.cpp
@@ -6,11 +6,11 @@ dsp::FunctionOscillator::FunctionOscillator(Type type)
     getInputs().push_back(phase);
 }
 
-std::function<dsp::Sample(dsp::Sample)> dsp::FunctionOscillator::getFunction() const {
+std::shared_ptr<std::function<dsp::Sample(dsp::Sample)>> dsp::FunctionOscillator::getFunction() const {
     return function;
 }
 
-void dsp::FunctionOscillator::setFunction(std::function<Sample(Sample)> function) {
+void dsp::FunctionOscillator::setFunction(std::shared_ptr<std::function<Sample(Sample)>> function) {
     this->function = function;
 }
 
@@ -19,12 +19,12 @@ std::shared_ptr<dsp::Input> dsp::FunctionOscillator::getPhase() const {
 }
 
 void dsp::FunctionOscillator::processNoLock() {
-    if (function) {
+    if (function != nullptr) {
         for (size_t channel = 0; channel < getNumChannels(); ++channel) {
             Sample *phaseChannel = getPhase()->getWrapper().getChannelPointer(channel);
             Sample *outputChannel = getOutput()->getWrapper().getChannelPointer(channel);
             for (size_t sample = 0; sample < getNumSamples(); ++sample) {
-                outputChannel[sample] = function(phaseChannel[sample]);
+                outputChannel[sample] = (*function)(phaseChannel[sample]);
             }
         }
     }
--- src/Nodes/Generators/FunctionOscillator.h
+++ src/Nodes/Generators/FunctionOscillator.h
@@ -9,8 +9,8 @@ class FunctionOscillator : public Producer {
 public:
     FunctionOscillator(Type type = Type::RATIO);
 
-    std::function<Sample(Sample)> getFunction() const;
-    void setFunction(std::function<Sample(Sample)> function);
+    std::shared_ptr<std::function<Sample(Sample)>> getFunction() const;
+    void setFunction(std::shared_ptr < std::function<Sample(Sample)>> function);
 
     std::shared_ptr<Input> getPhase() const;
 
@@ -18,7 +18,7 @@ protected:
     void processNoLock() override;
 
 private:
-    std::function<Sample(Sample)> function;
+    std::shared_ptr<std::function<Sample(Sample)>> function;
     const std::shared_ptr<Input> phase;
 };
 
